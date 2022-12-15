fdf745cb5b82eb7914fd20f0517ec0c4967beb8b
4aikovski <92368168+4aikovski@users.noreply.github.com>
2022-12-15 17:19:30 +0900

    Update tests.py

--- students/tests.py
+++ students/tests.py
@@ -1,25 +1,23 @@
 from django.test import TestCase
-from students.models import Student
+from students.models import student
+
 
 class TestStudent(TestCase):
     def test_short_str(self):
-        s = Student()
+        s = student()
         s.last_name = "Сидоров"
-        s.first_name = "Сидор"
-        s.number = "1234"
-
+        s.First_name = "Сидор"
+        s.numver = '1244'
         res = str(s)
-
-        self.assertEqual(res, "Сидоров С.")
+        self.assertEqual(res,"Сидоров С")
+# Create your tests here.
 
     def test_long_str(self):
-        s = Student()
+        s = student()
         s.last_name = "Сидоров"
-        s.first_name = "Сидор"
-        s.second_name = "Сидорович"
-        s.number = "1234"
-
+        s.First_name = "Сидор"
+        s.Second_name = "Сидорович"
+        s.numver = '1244'
         res = str(s)
-
-        self.assertEqual(res, "Сидоров С.С.")
+        self.assertEqual(res,"Сидоров СС")
 # Create your tests here.
